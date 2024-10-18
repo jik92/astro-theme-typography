5b5290754e628f25b0bba3415a03c111a09a1c83
Your Name <you@example.com>
2024-10-19 06:23:00 +0800

    [fixed] wrong

--- astro.config.ts
+++ astro.config.ts
@@ -1,59 +1,62 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import swup from '@swup/astro'
-import { defineConfig } from 'astro/config'
+import {defineConfig, passthroughImageService} from 'astro/config'
 import robotsTxt from 'astro-robots-txt'
 import UnoCSS from 'unocss/astro'
-import { themeConfig } from './src/.config'
+import {themeConfig} from './src/.config'
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
 
 // https://astro.build/config
 export default defineConfig({
-  site: themeConfig.site.website,
-  prefetch: true,
-  base: '/',
-  markdown: {
-    remarkPlugins: [
-      remarkMath,
-    ],
-    rehypePlugins: [
-      rehypeKatex,
-    ],
-    shikiConfig: {
-      theme: 'dracula',
-      wrap: true,
+    site: themeConfig.site.website,
+    prefetch: true,
+    base: '/',
+    markdown: {
+        remarkPlugins: [
+            remarkMath,
+        ],
+        rehypePlugins: [
+            rehypeKatex,
+        ],
+        shikiConfig: {
+            theme: 'dracula',
+            wrap: true,
+        },
     },
-  },
-  integrations: [
-    UnoCSS({ injectReset: true }),
-    mdx({
-      remarkPlugins: [
-        remarkMath,
-      ],
-      rehypePlugins: [
-        rehypeKatex,
-      ],
-    }),
-    robotsTxt(),
-    sitemap(),
-    swup({
-      theme: false,
-      animationClass: 'transition-swup-',
-      cache: true,
-      preload: true,
-      accessibility: true,
-      smoothScrolling: true,
-      updateHead: true,
-      updateBodyClass: true,
-    }),
-  ],
-  vite: {
-    server: {
-      watch: {
-        // avoiding WSL2 cant to reload it. - https://github.com/vitejs/vite/issues/5878
-        usePolling:true
-      },
+    integrations: [
+        UnoCSS({injectReset: true}),
+        mdx({
+            remarkPlugins: [
+                remarkMath,
+            ],
+            rehypePlugins: [
+                rehypeKatex,
+            ],
+        }),
+        robotsTxt(),
+        sitemap(),
+        swup({
+            theme: false,
+            animationClass: 'transition-swup-',
+            cache: true,
+            preload: true,
+            accessibility: true,
+            smoothScrolling: true,
+            updateHead: true,
+            updateBodyClass: true,
+        }),
+    ],
+    vite: {
+        server: {
+            watch: {
+                // avoiding WSL2 cant to reload it. - https://github.com/vitejs/vite/issues/5878
+                usePolling: true
+            },
+        },
     },
-  },
+    image: {
+        service: passthroughImageService(),
+    },
 })
